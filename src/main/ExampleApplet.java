5594f20f4faac8e60375008d6ec509fcbbdf7565
Jake Stothard <stothardj@gmail.com>
2011-02-26 21:16:50 -0800

    Load stadium

--- src/main/ExampleApplet.java
+++ src/main/ExampleApplet.java
@@ -3,16 +3,22 @@ package main;
 import java.applet.Applet;
 import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Iterator;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.GLU;
-import com.obj.*;
 
-public class ExampleApplet extends Applet {
+public class ExampleApplet extends Applet implements KeyListener {
 	
 	Canvas display_parent;
-	Box b;
+	Parser p;
+	Point3D camera;
+	
+	boolean up, down, left, right;
 	
 	/** Thread which runs the main game loop */
 	Thread gameThread;
@@ -71,7 +77,8 @@ public class ExampleApplet extends Applet {
 	
 	public void init() {
 		System.err.println("Program began");
-		WavefrontObject loadedWaveObject = new WavefrontObject("../models/stadium.obj");
+		
+		this.addKeyListener(this);
 		
 		setLayout(new BorderLayout());
 		try {
@@ -113,22 +120,85 @@ public class ExampleApplet extends Applet {
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glDepthFunc(GL11.GL_LEQUAL);
         GL11.glHint(GL11.GL_PERSPECTIVE_CORRECTION_HINT, GL11.GL_NICEST);
-        b = new Box();
+                
+        p = new Parser("../models/stadium.obj");
+        camera = new Point3D(0,0,0);
 	}
 	
     private void render(){
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT |GL11.GL_DEPTH_BUFFER_BIT);
-        b.draw();
+        
+        Iterator<Triangle> it = p.triangles.iterator();
+        int i = 0;
+        while(it.hasNext()) {
+        	i ++;
+        	it.next().draw(camera);
+        }
         GL11.glLoadIdentity();
     }
 
+    public void controlCamera() {
+    	if( up && !down)
+    		camera.y ++;
+    	else if( down && !up)
+    		camera.y --;
+    	if( left && !right)
+    		camera.x --;
+    	else if( right && !left)
+    		camera.x ++;
+    }
 	public void gameLoop() {
 		while(running) {
 			Display.sync(60);
 			render();
 			Display.update();
+			controlCamera();
 		}
 		
 		Display.destroy();
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.err.println("Key pressed");
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			up = true;
+			break;
+		case KeyEvent.VK_DOWN:
+			down = true;
+			break;
+		case KeyEvent.VK_LEFT:
+			left = true;
+			break;
+		case KeyEvent.VK_RIGHT:
+			right = true;
+			break;
+		}
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		System.err.println("Key released");
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			up = false;
+			break;
+		case KeyEvent.VK_DOWN:
+			down = false;
+			break;
+		case KeyEvent.VK_LEFT:
+			left = false;
+			break;
+		case KeyEvent.VK_RIGHT:
+			right = false;
+			break;
+		}		
 	}
 }
