79a37e5cc5e8d8db5119ce44128d4a552d015749
Brian Garfinkel <bagrm@myhost.localdomain>
2011-02-26 17:17:23 -0800

    Listener and Source classes along with test usage program.

---
+++ AudioPreview/src/main/AudienceMember.java
@@ -0,0 +1,91 @@
+package main;
+
+import java.nio.FloatBuffer;
+
+import org.lwjgl.openal.AL;
+import org.lwjgl.openal.AL10;
+
+public class AudienceMember {
+	/** Position of the listener. */
+	private FloatBuffer listenerPos;
+	/** Velocity of the listener. */
+	private FloatBuffer listenerVel;
+	/**
+	 * Orientation of the listener. (first 3 elements are "at", second 3 are
+	 * "up")
+	 */
+	private FloatBuffer listenerOri;
+
+	public AudienceMember(FloatBuffer listenerPos, FloatBuffer listenerVel,
+			FloatBuffer listenerOri) {
+		this.listenerPos = listenerPos;
+		this.listenerPos.rewind();
+		this.listenerVel = listenerVel;
+		this.listenerVel.rewind();
+		this.listenerOri = listenerOri;
+		this.listenerOri.rewind();
+		if (AL.isCreated()) {
+			this.setListenerValues();
+		}
+	}
+
+	public void setListenerPos(FloatBuffer listenerPos) {
+		this.listenerPos.rewind();
+		this.listenerPos = listenerPos;
+		this.listenerPos.rewind();
+	}
+
+	public void setListenerVel(FloatBuffer listenerVel) {
+		this.listenerVel.rewind();
+		this.listenerVel = listenerVel;
+		this.listenerVel.rewind();
+	}
+
+	public void setListenerOrientation(FloatBuffer listenerOri) {
+		this.listenerOri.rewind();
+		this.listenerOri = listenerOri;
+		this.listenerOri.rewind();
+	}
+	public void setListenerSPos(int index, float pos) {
+		this.listenerPos.rewind();
+		this.listenerPos.put(index, pos);
+		this.listenerPos.rewind();
+	}
+	public void setListenerSVel(int index, float vel) {
+		this.listenerVel.rewind();
+		this.listenerVel.put(index, vel);
+		this.listenerVel.rewind();
+	}
+	public void setListenerSOri(int index, float ori) {
+		this.listenerOri.rewind();
+		this.listenerOri.put(index, ori);
+		this.listenerOri.rewind();
+	}
+
+	void setListenerValues() {
+		AL10.alListener(AL10.AL_POSITION, listenerPos);
+		AL10.alListener(AL10.AL_VELOCITY, listenerVel);
+		AL10.alListener(AL10.AL_ORIENTATION, listenerOri);
+	}
+
+	public FloatBuffer getListenerPos() {
+		return this.listenerPos;
+	}
+
+	public FloatBuffer getListenerVel() {
+		return this.listenerVel;
+	}
+
+	public FloatBuffer getListenerOri() {
+		return this.listenerOri;
+	}
+	public float getListenerSPos(int index) {
+		return this.listenerPos.get(index);
+	}
+	public float getListenerSVel(int index) {
+		return this.listenerVel.get(index);
+	}
+	public float getListenerSOri(int index) {
+		return this.listenerOri.get(index);
+	}
+}
