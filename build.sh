15b87b8d90b875eac73d833c26ec97a7ce29393f
chi <azn.jalapeno@gmail.com>
2011-03-03 13:31:12 -0800

    some website stuff
    - also added the build.sh file to build up the project for website
    - website can run straight from the local directory, just open
    	index.html with browser (linux only right now)

---
+++ build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+JAR_LOC='../website/ss12ap.jar'
+SIGNER_PASSWORD='uclass12'
+SIGNER_STORE='uclass12'
+
+function fail_and_quit {
+  echo '***ERROR: ' $1
+  exit -1
+}
+
+echo "code goes in, jar comes out."
+printf "never a miscommunication\n\n"
+echo "***BUILDING SS12AudioPreview" 
+
+cd src
+
+if ! javac $(find ./ -iname "*.java")
+then
+  fail_and_quit "compilation"
+fi
+
+CLASS_FILES=$(find ./ -iname "*.class") 
+#AUDIO_FILES=$(find src -iname "*.wav")
+
+if ! jar cvf $JAR_LOC $CLASS_FILES #$AUDIO_FILES
+then
+  fail_and_quit "creating jar"
+fi
+
+if ! (echo $SIGNER_PASSWORD | jarsigner $JAR_LOC $SIGNER_STORE 2> /dev/null)
+then
+  fail_and_quit "signing jar"
+fi
+echo "***SUCCESS"
+echo "jar placed at " $JAR_LOC 
+
+if [ 'c' = "$1" ] 
+then
+  rm $CLASS_FILES
+  echo "***CLEANED .class files"
+fi
+
+
